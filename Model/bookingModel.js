bf9cb5a3e6ebcfbbb6ad99bafd34dd206b07b3b3
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-01-12 19:26:25 +0545

    Created Booking Schema

---
+++ Model/bookingModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema({
+    movie: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Movie',
+        required: [true, 'Booking must belong to a movie!']
+    },
+    user: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+        required: [true, 'Booking must belong to user!']
+    },
+    price: {
+        type: Number,
+        required: [true, 'Booking must have a price!']
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    paid: {
+        type: Boolean,
+        default: true
+    }
+});
+
+bookingSchema.pre(/^find/, function (next) {
+    this.populate('user').populate({
+        path: 'Movie',
+        select: 'movieName'
+    })
+})
+
+const Booking = mongoose.model('Booking', bookingSchema);
+module.exports = Booking;
