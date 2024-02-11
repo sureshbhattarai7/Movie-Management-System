f44345907385ab824fa39f4021aa22889c356cce
Suresh Bhattarai <bhattaraisuresh009@gmail.com>
2024-02-11 15:46:36 +0545

    Added Booking Controller

--- Controller/bookingController.js
+++ Controller/bookingController.js
@@ -1,51 +1,91 @@
-const Movie = require('./../Model/movieModel');
-const catchAsync = require('./../Utils/catchAsync');
-const AppError = require('./../Utils/appError');
-const Booking = require('../Model/bookingModel');
+const Booking = require("./../Model/bookingModel");
+const booking = require("./../Model/bookingModel");
+const Movie = require("./../Model/movieModel");
+const catchAsync = require("./../Utils/catchAsync");
 
+exports.createBooking = catchAsync(async (req, res, next) => {
+  const { movie, user, price, seatNo, showTime, BookingDate, amount, token } =
+    req.body;
 
-// exports.getCheckoutSession = catchAsync(async (req, res, next) => {
-//     const movie = await Movie.findById(req.params.MovieId);
+  let data = {
+    token: token,
+    amount: amount,
+  };
 
-//     if (!movie) {
-//         return next(new AppError('Can not find movie with that ID!'));
-//     }
+  let config = {
+    headers: {
+      Authorization: `key ${process.env.KHALTI_TEST_SECRET_KEY}`,
+    },
+  };
 
-//     //CREATE KHALTI INTEGRATION
-//     const totalAmount = `Total amount = ${Movie.price} for ${Movie.movieName}`;
-//     console.log(totalAmount);
-//     const header = {
-//         'Authorization': `key ${process.env.KHALTI_TEST_SECRET_KEY}`,
-//         'Content-type': 'application/json'
-//     }
-//     console.log(header);
+  console.log(data);
 
-//     const options = {
-//         return_url: '127.0.0.1:8000/api/v1/movies',
-//         website_url: '127.0.0.1:8000/api/v1/movies',
-//         price: Movie.price * 100,
-//         purchase_order_id: booking._id,
-//         purchase_order_name: req.user.fullName,
-//         customer_info: {
-//             'name': req.user.fullName,
-//             'email': req.user.email
-//         },
-//     }
-//     console.log(options);
-//});
+  const finalPayment = await axios.post(
+    "https://khalti.com/api/v2/payment/verify/",
+    data,
+    config
+  );
+  console.log(`Final payment is: ${finalPayment.data}`);
 
-exports.getMyMovies = async(req, res, next) => {
-    //Find all bookings
-    const bookings = await Booking.find({ user: req.user.id });
+  const bookData = await Booking.create({
+    movie: finalPayment.data.product_identity,
+    user: req.user._id,
+    price,
+    seatNo,
+    bookingDate,
+    showTime,
+    transactionId: finalPayment.data.idx,
+  });
 
-    //Find movies with the returned IDs 
-    const movieIDs = bookings.map(el => el.movie);
-    const movies = await Movie.find({ _id: { $in: movieIDs } });
+  res.status(200).json({
+    status: "success",
+    data: bookData,
+  });
+});
 
+exports.getBooking = catchAsync(async (req, res, next) => {
+  const data = await Booking.find();
+  res.statu(200).json({
+    status: "success",
+    data,
+  });
+});
+
+exports.checkSeatAvailability = catchAsync(async (req, res, next) => {
+  const { movieName, date, time } = req.body;
+
+  const movie = await Movie.findOne({ _id: movieName });
+  console.log(date, time);
+
+  if (!movie) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Movie not found!",
+    });
+  }
+
+  const pipeLine = [
+    {
+      $match: {
+        movie: movie._id,
+        bookingDate: { $eq: date },
+        showTime: { $eq: time },
+      },
+    },
+    {
+      $unwind: "$seatNo",
+    },
+      {
+          $project: {
+              seatNo: 1,
+              movie: 1
+        }
+    },
+    ];
+    
+    const result = await Booking.aggregate(pipeline);
     res.status(200).json({
         status: 'success',
-        data: {
-            movies
-        }
+        data: result
     })
-}
+});
